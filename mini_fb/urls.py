5db05f0f01a773eb94fbd2bd64123af312fa72cd
Tim Marro <tmarro@bu.edu>
2024-10-15 06:55:15 -0400

    fix

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -2,14 +2,14 @@ from django.urls import path
 from django.conf import settings
 from .views import (
     ShowAllProfilesView,
-    ShowProfilePageView,
+    ShowProfileView,
     CreateProfileView,
     CreateStatusMessageView,
 )
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
-    path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
+    path('profile/<int:pk>/', ShowProfileView.as_view(), name='show_profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
     path('profile/<int:pk>/create_status/', CreateStatusMessageView.as_view(), name='create_status'),
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -9,9 +9,9 @@ class ShowAllProfilesView(ListView):
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
 
-class ShowProfilePageView(DetailView):
+class ShowProfileView(DetailView):
     model = Profile
-    template_name = 'mini_fb/show_profile_page.html' 
+    template_name = 'mini_fb/show_profile.html' 
     context_object_name = 'profile'  
 
 class CreateProfileView(CreateView):
